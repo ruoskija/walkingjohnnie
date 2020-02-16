f57767e299367816aad9a4107a9784fd7951ed74
ruoskija <16821766+ruoskija@users.noreply.github.com>
2020-02-16 18:25:41 +0200

    Add JSDoc comments to agent.js

--- modules/agent.js
+++ modules/agent.js
@@ -1,6 +1,14 @@
 import { randN } from './helpers.js';
 
+/** Class representing an agent. */
 class Agent {
+    /**
+     * Creates an agent.
+     * @param {number} x - starting x coordinate 
+     * @param {number} y - starting y coordinate
+     * @param {string} trailColor - css color of the trail the agent leaves behind
+     * @param {string} color - css color of the agent
+     */
     constructor(x=0, y=0, trailColor='#609609', color='#070707') {
         this.x = x;
         this.y = y;
@@ -8,6 +16,7 @@ class Agent {
         this.trailColor = trailColor;
     }
 
+    /** Randomly moves the agent to a neighbor coordinate */
     step() {
         switch (randN(4)) {
             case 0:
@@ -24,13 +33,27 @@ class Agent {
         }
     }
 
+    /**
+     * Check if the agent is outside a given area. 
+     * The area starts from coordinates (0, 0).
+     * @param {number} areaWidth 
+     * @param {number} areaHeight 
+     * @return {boolean} - true if agent is outside of the given area
+     */
     isOutsideArea(areaWidth, areaHeight) {
         return (this.x < 0          ||
                 this.x >= areaWidth ||
                 this.y < 0          ||
                 this.y >= areaHeight);
     }
-
+    
+    /**
+     * Check if the agent is inside a given area. 
+     * The area starts from coordinates (0, 0).
+     * @param {number} areaWidth 
+     * @param {number} areaHeight 
+     * @return {boolean} - true if agent is inside of the given area
+     */
     isInsideArea(areaWidth, areaHeight) {
         return !this.isOutsideArea(areaWidth, areaHeight);
     }
