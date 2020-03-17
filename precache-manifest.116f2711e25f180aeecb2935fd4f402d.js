43d25e7e258a4b1d06460094fbf413331805351a
ruoskija <16821766+ruoskija@users.noreply.github.com>
2020-03-17 16:12:06 +0200

    Updates

---
+++ precache-manifest.116f2711e25f180aeecb2935fd4f402d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dda2b2e988d1595e7b11f3008308ca0",
+    "url": "/walkingjohnnie/index.html"
+  },
+  {
+    "revision": "bfe59e4147698f383d87",
+    "url": "/walkingjohnnie/static/css/main.d1f55de0.chunk.css"
+  },
+  {
+    "revision": "41907fe3c3b19767565b",
+    "url": "/walkingjohnnie/static/js/2.cfc0e2d9.chunk.js"
+  },
+  {
+    "revision": "949611f89efac64adf70580a2b7f5ead",
+    "url": "/walkingjohnnie/static/js/2.cfc0e2d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe59e4147698f383d87",
+    "url": "/walkingjohnnie/static/js/main.9927e194.chunk.js"
+  },
+  {
+    "revision": "c38ccb84e696703d4203",
+    "url": "/walkingjohnnie/static/js/runtime-main.d3f913df.js"
+  }
+]);
